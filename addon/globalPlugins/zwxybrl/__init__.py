5d9609a281e490a93682b8334bc394b7f4f4e13c
Bo-Cheng Jhan <school510587@yahoo.com.tw>
2024-10-19 01:24:07 +0800

    Use XingYi character descriptions iff zwxybrl.ctb is working

--- addon/globalPlugins/zwxybrl/__init__.py
+++ addon/globalPlugins/zwxybrl/__init__.py
@@ -2,7 +2,9 @@
 
 import os
 
+from brailleTables import getTable as getBRLtable
 from logHandler import log
+import braille
 import characterProcessing
 import globalPluginHandler
 import globalVars
@@ -18,7 +20,22 @@ class CustomCharacterDescriptions(characterProcessing.CharacterDescriptions):
         finally:
             globalVars.appDir = old_appDir
 
+class CustomLocaleDataMap(characterProcessing.LocaleDataMap):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._xyCharDescLocaleDataMap = characterProcessing.LocaleDataMap(CustomCharacterDescriptions)
+    def fetchLocaleData(self, locale: str, *args, **kwargs):
+        try:
+            if locale == "zh_TW" and braille.handler.table is getBRLtable("zwxybrl.ctb"):
+                return self._xyCharDescLocaleDataMap.fetchLocaleData(locale, *args, **kwargs)
+        except:
+            log.error("Failed to read XingYi character descriptions", exc_info=True)
+        return super().fetchLocaleData(locale, *args, **kwargs)
+    def invalidateLocaleData(self, locale: str, *args, **kwargs):
+        self._xyCharDescLocaleDataMap.invalidateLocaleData(locale, *args, **kwargs)
+        return super().invalidateLocaleData(locale, *args, **kwargs)
+
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     def __init__(self, *args, **kwargs):
         super(GlobalPlugin, self).__init__(*args, **kwargs)
-        characterProcessing._charDescLocaleDataMap = characterProcessing.LocaleDataMap(CustomCharacterDescriptions)
+        characterProcessing._charDescLocaleDataMap = CustomLocaleDataMap(characterProcessing.CharacterDescriptions)
